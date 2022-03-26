ca76e149cdf45ffe5d2fcc783eaaa8f6c5df9e81
Khongkong <conscience1113@gmail.com>
2022-03-26 13:58:22 +0800

    Simplify constructors

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -7,13 +7,11 @@ namespace GildedRose;
 final class GildedRose
 {
     /**
-     * @var Item[]
+     * @param Item[] $items
      */
-    private $items;
-
-    public function __construct(array $items)
-    {
-        $this->items = $items;
+    public function __construct(
+        private array $items
+    ){
     }
 
     public function updateQuality(): void
--- php/src/Item.php
+++ php/src/Item.php
@@ -6,26 +6,11 @@ namespace GildedRose;
 
 final class Item
 {
-    /**
-     * @var string
-     */
-    public $name;
-
-    /**
-     * @var int
-     */
-    public $sellIn;
-
-    /**
-     * @var int
-     */
-    public $quality;
-
-    public function __construct(string $name, int $sellIn, int $quality)
-    {
-        $this->name = $name;
-        $this->sellIn = $sellIn;
-        $this->quality = $quality;
+    public function __construct(
+        public string $name, 
+        public int $sellIn, 
+        public int $quality
+    ) {
     }
 
     public function __toString(): string
